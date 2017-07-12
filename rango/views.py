93da1e69a41984cc6d9c981a6124cfa7e9b6efee
Administrator <admin@example.com>
2017-07-12 09:36:39 +0800

    TWD chapter 1-11

--- rango/views.py
+++ rango/views.py
@@ -4,17 +4,50 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = get_server_side_cookie(request, 'visits', '1')
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+
+    if (datetime.now() - last_visit_time).seconds > 5:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # visits = 1
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
 
 
 def index(request):
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
+
+    visitor_cookie_handler(request)
+
+    context_dict['visits'] = request.session['visits']
+
     return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+    return render(request, 'rango/about.html', {'visits': count})
 
 
 def category(request, category_name_slug):
